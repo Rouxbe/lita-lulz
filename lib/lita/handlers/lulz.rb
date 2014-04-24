f0c46adaaa0eabf6197a8ead7717af01f0ba97a0
Adam O'Connor <northband@gmail.com>
2014-04-24 12:23:45 -0400

    Change url to BASE_URL and remove some help text

--- lib/lita/handlers/lulz.rb
+++ lib/lita/handlers/lulz.rb
@@ -5,19 +5,19 @@ module Lita
   module Handlers
     class Lulz < Handler
       route(/\blulz\b/i, :lulz, command: true, help: { "lulz" => "Display a single image" })
-      route(/\blulz bomb( (\d+))?/i, :bomb, command: true, help: { "lulz bomb" => "Display five images", "lulz bomb N" => "Display N lulz" })
+      route(/\blulz bomb( (\d+))?/i, :bomb, command: true, help: { "lulz bomb" => "Display five images" })
 
       BASE_URL = "http://bukk.it"
 
       def lulz(response)
-        doc = Nokogiri::HTML(open(url))
+        doc = Nokogiri::HTML(open(BASE_URL))
         images = doc.xpath("//a").to_a[5..-1]
 
         response.reply url + "/" + images.sample
       end
 
       def bomb(response)
-        doc = Nokogiri::HTML(open(url))
+        doc = Nokogiri::HTML(open(BASE_URL))
         images = doc.xpath("//a").to_a[5..-1]
 
         images.each do |image|
